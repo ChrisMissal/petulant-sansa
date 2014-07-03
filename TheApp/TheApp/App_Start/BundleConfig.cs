1ecf3afe5616524152ab14b24958e9af44163e14
Chris Missal <chris.missal@gmail.com>
2014-07-02 22:40:21 -0500

    initial boilerplate code

---
+++ TheApp/TheApp/App_Start/BundleConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace TheApp
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/bundles/base").Include(
+                        "~/Content/css/base.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                        "~/Scripts/angular.js",
+                        "~/Scripts/angular-*"
+                        ));
+        }
+    }
+}
